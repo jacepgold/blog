c69a17c09c9d59a5d39668ee8c3cd86b92b9e919
Jace P. Gold <Jace@JaceGold.com>
2017-11-27 14:08:29 -0700

    CRUD Actions Complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,14 +6,27 @@ class PagesController < ApplicationController
 
   def show
     # Single Blog Page
+    @page = Page.find(params[:id])
   end
 
   def edit
     # Shows a pre filled form to edit the page
+    @page = Page.find(params[:id])
   end
 
   def update
     # Take data from the form and update the page in our database
+    @page = Page.find(params[:id])
+    if @page.update(title: params[:page][:title],
+                    author: params[:page][:author],
+                    content: params[:page][:content],
+                    public: params[:page][:public])
+      redirect_to page_path(@page)
+      # Successful update
+    else
+      # Failed update
+      render :edit
+    end
   end
 
   def new
@@ -23,9 +36,22 @@ class PagesController < ApplicationController
 
   def create
     # Take data from the form and create a new page in our database
+    @page = Page.new(title: params[:page][:title],
+                    author: params[:page][:author],
+                    content: params[:page][:content],
+                    public: params[:page][:public])
+    if @page.save
+      # Success
+      redirect_to pages_path
+    else
+      # Fail
+      render :new
+    end
   end
 
   def destroy
     # Finds the page by ID and removes it from the database
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
   end
 end
